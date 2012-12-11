76ec3b81857e4c1958cf3fdeb68989f199aad30f
Philippe Dionne <dionne.phil@gmail.com>
2012-12-11 13:39:51 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'capybara'
+require 'capybara/dsl'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://www.facebook.com'
+
+module UnfrienderApp
+
+  class Unfriender
+    include Capybara::DSL
+
+    def unfriend!(friend_id_list)
+      login
+
+      friend_id_list.each do |friend_id|
+        find_friend_by_id(friend_id)
+        unfriend
+        p "Goodbye #{friend_id} !"
+      end
+    end
+
+    def login
+      visit('/')
+      within('#login_form') do
+        fill_in('email', :with => ENV['FACEBOOK_USERNAME'])
+        fill_in('pass', :with => ENV['FACEBOOK_PASSWORD'])
+        find('label#loginbutton input[type="submit"]').click
+      end
+    end
+
+    def find_friend_by_id(friend_id)
+      visit("/#{friend_id}")
+    end
+
+    def unfriend
+      find('div.actionsDropdown .wrap a.fbTimelineActionSelectorButton').click
+      find('li#profile_action_remove_friend a.itemAnchor').click
+      find('#pop_content input[name="remove-friend"]').click
+    end
+
+  end # Unfriender
+end # UnfrienderApp
+
+friend_id_list = ['']
+
+unfriender = UnfrienderApp::Unfriender.new
+unfriender.unfriend!(friend_id_list)
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Unfriender
+
+## Getting started
+1. Get all your facebook friends: `https://graph.facebook.com/me/friends?access_token=ACCESS_TOKEN` and select the ids of those you'd like to unfriend
+2. Set FACEBOOK_USERNAME and FACEBOOK_PASSWORD environement variables.
+3. Run:
+
+```
+bundle install
+ruby "app.rb"
+```
+
+---
+
+# Author
+[Philippe Dionne](http://phildionne.com)
+
+# Copyright and license
+
+Copyright 2012 Philippe Dionne
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
